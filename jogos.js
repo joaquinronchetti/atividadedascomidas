3a7ea63d3d663839e0b321a7425dfd46b40fbbc4
joaquinronchetti <joaquin_ronchetti@estudante.sesisenai.org.br>
2026-06-17 08:43:52 -0300

    Add files via upload

---
+++ jogos.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercício JavaScript</title>
+</head>
+<body>
+    <script>
+        let jogos = [
+            "GTA V",
+            "Red Dead Redemption 2",
+            "The Last of Us",
+            "God of War",
+            "Call of Duty"
+        ];
+
+        console.log("Meus Jogos Favoritos");
+
+        for (let jogo of jogos) {
+            console.log(jogo);
+        }
+    </script>
+</body>
+</html>
