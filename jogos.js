9eb90b8ae819880cb0af3c5fd788d3ff2dfcf771
joaquinronchetti <joaquin_ronchetti@estudante.sesisenai.org.br>
2026-06-17 08:45:39 -0300

    Add files via upload

---
+++ jogos.js
@@ -0,0 +1,34 @@
+// Exercício 1
+let comidas = [
+    "Pizza",
+    "Hambúrguer",
+    "Lasanha",
+    "Pastel",
+    "Sorvete"
+];
+
+for (let comida of comidas) {
+    console.log("Eu gosto muito de", comida);
+}
+
+// Exercício 2
+let numeros = [10, 20, 30, 40, 50];
+
+for (let numero of numeros) {
+    console.log("Número encontrado:", numero);
+}
+
+// Projeto Final
+let jogos = [
+    "GTA V",
+    "Red Dead Redemption 2",
+    "The Last of Us",
+    "God of War",
+    "Call of Duty"
+];
+
+console.log("Meus Jogos Favoritos");
+
+for (let jogo of jogos) {
+    console.log(jogo);
+}
